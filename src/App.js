4a51834471c296a5849a79f44f7e065eabd7bdf0
Irvan Malik <irvanmalik48@gmail.com>
2022-04-20 01:44:49 +0700

    Dumb me for not knowing that I can use `react-reveal`
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,18 +4,8 @@ import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Explore from "./Pages/Explore";
 import Details from "./Pages/Details";
-import { useEffect } from "react";
 
 function App() {
-    useEffect(() => {
-        const script = document.createElement('script');
-
-        script.src = "/sts.js"
-        script.defer = true;
-
-        document.body.appendChild(script);
-    }, []);
-
     return (
         <BrowserRouter>
             <Navbar/>
