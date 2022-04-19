905e54a4e009578d398dd13b3cc4c59353a203b3
Irvan Malik <irvanmalik48@gmail.com>
2022-04-20 01:12:29 +0700

    Extends animation and keyframes config
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
             fontFamily: {
                 'sans': ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
             },
+            animation: {
+                'fadein': 'fadeIn 1s ease-out forwards',
+            },
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: 0, transform: "translateY(2rem)" },
+                    '100%': { opacity: 1, transform: "translateY(0)" }, 
+                },
+            },
         },
     },
     plugins: [],
