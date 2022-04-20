b963ed6b00323d5b99db6e7d131c03bfb0616cc0
Irvan Malik <irvanmalik48@gmail.com>
2022-04-20 13:48:48 +0700

    Dark mode and animation for about us
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/Components/ProfileSection.js
+++ src/Components/ProfileSection.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fade } from 'react-reveal';
 import Button from "./Button";
 
 function ProfileSection(props) {
@@ -6,13 +7,13 @@ function ProfileSection(props) {
         <div className="container">
             <div className="mt-8 md:mt-12 lg:mt-20 max-w-lg mx-auto lg:max-w-full lg:grid lg:grid-cols-12">
                 <div className="mb-14 lg:mb-0 lg:col-span-5 md:pr-12 xl:pr-32">
-                    <h2 className="text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold text-secondary leading-normal">The creative people behind <span
-                        className="text-primary leading-normal">Bali Province</span>.
+                    <h2 className="text-2xl md:text-4xl lg:text-3xl xl:text-4xl font-bold text-secondary dark:text-blue-300 leading-normal">The creative people behind <span
+                        className="text-primary dark:text-red-300 leading-normal">Bali Province</span>.
                     </h2>
 
                     <div className="mt-10">
-                        <h3 className="md:text-2xl lg:text-lg xl:text-xl font-semibold text-secondary">Our Main Goals</h3>
-                        <p className="text-gray-600 font-light mt-3 text-sm llg:text-sm">Lorem ipsum dolor sit amet, consectetur
+                        <h3 className="md:text-2xl lg:text-lg xl:text-xl font-semibold text-secondary dark:text-blue-300">Our Main Goals</h3>
+                        <p className="text-gray-600 dark:text-gray-200 font-light mt-3 text-sm llg:text-sm">Lorem ipsum dolor sit amet, consectetur
                             adipisicing elit. Adipisci aliquam distinctio eaque esse
                             fugiat illum in, ipsam iste iusto laboriosam magnam nam nisi odit omnis quaerat quos
                             repellendus
@@ -20,27 +21,29 @@ function ProfileSection(props) {
                     </div>
 
                     <div className="mt-10">
-                        <h3 className="md:text-2xl lg:text-lg xl:text-xl font-semibold text-secondary">How to Support Us</h3>
-                        <p className="text-gray-600 font-light mt-3 text-sm lg:text-sm">Lorem ipsum dolor sit amet, consectetur
+                        <h3 className="md:text-2xl lg:text-lg xl:text-xl font-semibold text-secondary dark:text-blue-300">How to Support Us</h3>
+                        <p className="text-gray-600 dark:text-gray-200 font-light mt-3 text-sm lg:text-sm">Lorem ipsum dolor sit amet, consectetur
                             adipisicing elit. Beatae iure quasi vero voluptatum. Beatae inventore neque nostrum numquam
                             tempore vero!</p>
                     </div>
                 </div>
                 <div className="lg:col-span-7">
-                    <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
-                        {props.data.map((profile, index) => {
-                            return (
-                                <Button key={profile.id} type="link" href={profile.link} className="mb-5 group">
-                                    <img src={`/images/${profile.image_url}`} alt={profile.name}
-                                         className="object-cover transition group-hover:opacity-80"/>
-                                    <div className="text-center mt-3">
-                                        <h5 className="text-sm xl:text-base text-secondary font-medium group-hover:underline">{profile.name}</h5>
-                                        <p className="text-xs xl:text-base text-gray-600 text-sm mt-1">{profile.title}</p>
-                                    </div>
-                                </Button>
-                            )
-                        })}
-                    </div>
+                    <Fade bottom cascade>
+                        <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+                            {props.data.map((profile, index) => {
+                                return (
+                                    <Button key={profile.id} type="link" href={profile.link} className="mb-5 group">
+                                        <img src={`/images/${profile.image_url}`} alt={profile.name}
+                                            className="object-cover transition group-hover:opacity-80"/>
+                                        <div className="text-center mt-3">
+                                            <h5 className="text-sm xl:text-base text-secondary dark:text-blue-300 font-medium group-hover:underline">{profile.name}</h5>
+                                            <p className="xl:text-base text-gray-600 dark:text-gray-200 text-sm mt-1">{profile.title}</p>
+                                        </div>
+                                    </Button>
+                                )
+                            })}
+                        </div>
+                    </Fade>
                 </div>
             </div>
         </div>
--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -3,21 +3,21 @@ import React from 'react';
 function Details(props) {
     return (
         <div className="container mt-5 md:mt-10">
-            <h1 className="text-3xl lg:text-4xl font-semibold text-secondary">Pura Uluwatu</h1>
-            <h6 className="text-sm md:text-base text-gray-500 mt-2">Denpasar, Bali</h6>
+            <h1 className="text-3xl lg:text-4xl font-semibold text-secondary dark:text-blue-300">Pura Uluwatu</h1>
+            <h6 className="text-sm md:text-base text-gray-500 dark:text-gray-300 mt-2">Denpasar, Bali</h6>
             <img src="/images/popular-2.jpg" alt="" className="mt-5 w-full object-cover h-72 md:h-96"/>
             <div className="mt-12 md:max-w-3xl">
                 <h5 className="text-lg font-semibold">Lorem Ipsum</h5>
-                <p className="mt-3 text-gray-700 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur delectus deserunt
+                <p className="mt-3 text-gray-700 dark:text-gray-200 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur delectus deserunt
                     dolor, doloremque dolores fugit ipsam maxime minima, nostrum praesentium provident reiciendis suscipit
                     velit voluptatibus voluptatum? Nobis, quis?</p>
 
-                <p className="mt-3 text-gray-700 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur delectus deserunt
+                <p className="mt-3 text-gray-700 dark:text-gray-200 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur delectus deserunt
                     dolor, doloremque dolores fugit ipsam maxime minima, nostrum praesentium provident reiciendis suscipit
                     velit voluptatibus voluptatum? Nobis, quis?</p>
 
                 <h5 className="mt-10 text-lg font-semibold">Lorem ipsum dolor sit amet, consectetur adipisicing.</h5>
-                <p className="mt-3 text-gray-700 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur delectus deserunt
+                <p className="mt-3 text-gray-700 dark:text-gray-200 text-sm md:text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae consequatur delectus deserunt
                     dolor, doloremque dolores fugit ipsam maxime minima.?</p>
             </div>
         </div>
